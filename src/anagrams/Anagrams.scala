b3352d5b4c8fe1fc8219b8e48f5c06a4fe1adb8d
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-03-31 15:35:50 +0200

    Assignement version

---
+++ src/anagrams/Anagrams.scala
@@ -0,0 +1,69 @@
+package anagrams
+
+/**
+ * Sentence anagrams generation
+ * MSE course, T-AdvPrPa course
+ */
+object Anagrams {
+	/**
+	 * Types definitions
+	 */
+	type Word = String
+	type Occurrences = List[(Char, Int)]
+	type Sentence = List[Word]
+
+	/**
+	 * The dictionary is simply a sequence of words.
+	 * It is predefined and obtained as a sequence using the utility method `loadDictionary`.
+	 */
+	val dictionary: List[Word] = loadDictionary()
+
+	/**
+	 *  Question 1 : converts the word into its character occurrence list.
+	 *
+	 *  For instance,
+	 *  wordOccurences("abcd") gives List(('a', 1), ('b', 1), ('c', 1), ('d', 1))
+	 *  wordOccurences("aabcddd") gives List(('a', 2), ('b', 1), ('c', 1), ('d', 3))
+	 *
+	 *  Note: the upper case and lower case version of the character are treated as the
+	 *  same character, and are represented as a lower case character in the occurrence list.
+	 */
+	def wordOccurrences(w: Word): Occurrences = ???
+
+	/**
+	 * Question 2: Valid words
+	 * 
+	 * The `dictionaryByOccurrences` is a `Map` from different occurrences to a sequence of all
+	 * the words that have that occurrence count. This map serves as an easy way to obtain all the anagrams of a word given its occurrence list.
+	 */
+	lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = ???
+
+	/**
+	 * Question 3: Returns all the anagrams of a given word
+	 */	
+	def wordAnagrams(word: Word): List[Word] = ???
+	
+	/**
+	 * Question 4: Returns all the subsets of an occurrence list
+	 */
+
+	/**
+	 * Generates all the subsets of a set
+	 */
+	def combinations(occurrences: Occurrences): List[Occurrences] = ???
+	
+	/**
+	 * Question 5: remove occurrences from x that are in y
+	 */
+	def subtract(x: Occurrences, y: Occurrences): Occurrences = ???
+	
+	/**
+	 * Question 6 - Generate sentence anagrams
+	 */
+	
+	/** Converts a sentence into its character occurrence list. */
+	def sentenceOccurrences(s: Sentence): Occurrences = ???
+	
+	def sentenceAnagrams(sentence: Sentence): List[Sentence] = ???
+
+}
---
+++ src/anagrams/package.scala
@@ -0,0 +1,29 @@
+import java.io.File
+
+/**
+ * Sentence anagrams generation
+ * MSE course, T-AdvPrPa course
+ */
+package object anagrams {
+
+	/**
+	 * Loads a file dictionary which should be located in the src directory
+	 */
+	def loadDictionary() = {
+		val wordstream = Option {
+			getClass.getClassLoader.getResourceAsStream("resources/linuxwords.txt")
+		} getOrElse {
+			sys.error("Could not load word list, dictionary file not found")
+		}
+		try {
+			val s = io.Source.fromInputStream(wordstream)
+			s.getLines.toList
+		} catch {
+			case e: Exception ⇒
+				println("Could not load word list: " + e)
+				throw e
+		} finally {
+			wordstream.close()
+		}
+	}
+}
